171a43034a8d2b8f218c3910c066db9c7ce1bd9e
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-05-03 12:36:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -501,8 +501,8 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:
-                cc = f'**✨ {name1}[{CR}].mkv\n\n📚𝐁𝐀𝐓𝐂𝐇 〓 {b_name}\n\n**♛ 𝗕𝗬 😈 »» 𝗛𝗔𝗖𝗞𝗛𝗘𝗜𝗦𝗧 ♡**'
-                cc1 = f'**✨ {name1}[{CR}].mkv\n\n📚𝐁𝐀𝐓𝐂𝐇 〓 {b_name}\n\n**♛ 𝗕𝗬 😈 »» 𝗛𝗔𝗖𝗞𝗛𝗘𝗜𝗦𝗧 ♡**'
+                cc = f'**✨ {name1}[{CR}].mkv\n\n📚𝐁𝐀𝐓𝐂𝐇 〓 {b_name}\n\n♛ 𝗕𝗬 😈 »» 𝗛𝗔𝗖𝗞𝗛𝗘𝗜𝗦𝗧 ♡**'
+                cc1 = f'**✨ {name1}[{CR}].mkv\n\n📚𝐁𝐀𝐓𝐂𝐇 〓 {b_name}\n\n♛ 𝗕𝗬 😈 »» 𝗛𝗔𝗖𝗞𝗛𝗘𝗜𝗦𝗧 ♡**'
                 cczip = f'[——— ✦ {str(count).zfill(3)} ✦ ———]({link0})\n\n**📁 Title :** `{name1}`\n**├── Extention :**  {CR} .zip\n\n**📚 Course :** {b_name}\n\n**🌟 Extracted By :** {CR}'
                 ccimg = f'[——— ✦ {str(count).zfill(3)} ✦ ———]({link0})\n\n**🖼️ Title :** `{name1}`\n**├── Extention :**  {CR} .jpg\n\n**📚 Course :** {b_name}\n\n**🌟 Extracted By :** {CR}'
                 ccm = f'[——— ✦ {str(count).zfill(3)} ✦ ———]({link0})\n\n**🎵 Title :** `{name1}`\n**├── Extention :**  {CR} .mp3\n\n**📚 Course :** {b_name}\n\n**🌟 Extracted By :** {CR}'
